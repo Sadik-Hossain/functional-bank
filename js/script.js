45715e17ad93f9a150276f784af96bce8f6515b7
sadik <po480373@gmai.com>
2022-02-18 15:24:21 +0600

    updated withdraw condition

--- js/script.js
+++ js/script.js
@@ -7,51 +7,49 @@ const BalanceText = document.getElementById("balance-total");
 
 //--------------Handle Deposit---------------------------
 document.getElementById("deposit-btn").addEventListener("click", function () {
-  // storing given input value
-  let amount = getValue(depositInput);
+    // storing given input value
+    let amount = getValue(depositInput);
 
-  if (amount > 0) {
-    // updating deposit text
-    updateTotal(DepositText, amount);
-    // updating balance
-    updateBalance(amount, true);
-  }
+    if (amount > 0) {
+        // updating deposit text
+        updateTotal(DepositText, amount);
+        // updating balance
+        updateBalance(amount, true);
+    }
 });
 
 //----------------Handle Withdraw-----------------------------
 document.getElementById("withdraw-btn").addEventListener("click", function () {
-  // storing given input value
-  let amount = getValue(withdrawInput);
-
-  if (amount <= parseFloat(BalanceText.innerText)) {
-    if (amount > 0) {
-      // updating withdraw text
-      updateTotal(withdrawText, amount);
-      // updating balance
-      updateBalance(amount, false);
+    // storing given input value
+    let amount = getValue(withdrawInput);
+    //withdraw validation
+    if (amount > 0 && amount <= parseFloat(BalanceText.innerText)) {
+        // updating withdraw text
+        updateTotal(withdrawText, amount);
+        // updating balance
+        updateBalance(amount, false);
     }
-  }
 });
 //-------------Functions-------------------------------------
 
 //input value getting function
 function getValue(fieldId) {
-  inputValueNumber = parseFloat(fieldId.value);
-  fieldId.value = "";
-  return inputValueNumber;
+    inputValueNumber = parseFloat(fieldId.value);
+    fieldId.value = "";
+    return inputValueNumber;
 }
 
 //updating depo & withdraw text
 function updateTotal(fieldId, amount) {
-  fieldId.innerText = parseFloat(fieldId.innerText) + amount;
+    fieldId.innerText = parseFloat(fieldId.innerText) + amount;
 }
 
 //---------update balance-------------------------------
 function updateBalance(amount, isAdding) {
-  previousBalance = parseFloat(BalanceText.innerText);
-  if (isAdding == true) {
-    BalanceText.innerText = previousBalance + amount;
-  } else {
-    BalanceText.innerText = previousBalance - amount;
-  }
+    previousBalance = parseFloat(BalanceText.innerText);
+    if (isAdding == true) {
+        BalanceText.innerText = previousBalance + amount;
+    } else {
+        BalanceText.innerText = previousBalance - amount;
+    }
 }
